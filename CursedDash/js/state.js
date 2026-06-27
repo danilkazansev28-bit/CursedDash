b46f5588a37760877c55b3162b883bed802a010e
danilkazansev28-bit <danilkazansev28@gmail.com>
2026-06-27 13:59:37 +0500

    Update state.js

--- CursedDash/js/state.js
+++ CursedDash/js/state.js
@@ -1,8 +1,8 @@
 // js/state.js
 if (!window.Game) window.Game = {};
 
-window.Game.GRAVITY_CUBE = 0.4; 
-window.Game.JUMP_CUBE = -11;
+window.Game.GRAVITY_CUBE = 0.5; 
+window.Game.JUMP_CUBE = -10;
 window.Game.GRAVITY_SHIP = 0.25; 
 window.Game.THRUST_SHIP = -0.5; 
 window.Game.CUBE_SIZE = 40;
